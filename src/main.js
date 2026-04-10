bbd6ea20cc98940669ffdcc0552deadce1702e75
Luiz-Reis-Neves <luizreisneves@outlook.com>
2026-04-10 14:09:36 -0300

    eventos de botões add

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './styles/style.css';
+// Declaração de variaveis dos botões
+const botoesNav = document.querySelectorAll("#lista-navegacao button")
+
+// add eventos de click para cada botão
+botoesNav.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        console.log(botao.id)
+    })
+})
